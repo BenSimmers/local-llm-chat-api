0011bf7f276282d896ba1f014a7f4c905934b99a
Benjamin Simmers <bej1380@gmail.com>
2025-03-05 23:19:01 +1000

    chore: update .gitignore

--- dist/messageService.d.ts
+++
@@ -1,6 +0,0 @@
-import type { CoreMessage } from 'ai';
-export default class MessageService {
-  private messageHistory;
-  getMessageHistory(): CoreMessage[];
-  writeMessageHistory(messages: CoreMessage[]): void;
-}
